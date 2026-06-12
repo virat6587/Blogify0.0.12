5d88c1e05b1177ceb530308e1a1d87fbc820f07f
Virat <vishantvelip38@gmail.com>
2026-06-12 12:40:47 +0530

    Update authentication.js

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,47 +1,30 @@
-// middlewares/authentication.js
 const { verifyToken } = require("../services/authentication");
 
 const checkForAuthenticationCookie = (cookieName) => {
-    return (req, res, next) => {
-        const token = req.cookies[cookieName];
-        if (!token) {
-            req.user = null;
-            return next();
-        }
-
-        try {
-            const user = verifyToken(token);
-            req.user = user;
-        } catch (error) {
-            req.user = null;
-        }
-        next();
-    };
-};
-
-// Restrict to Logged-in Users Only
-const restrictToLoggedInUserOnly = (req, res, next) => {
-    if (!req.user) {
-        return res.redirect("/user/signin");
-    }
+  return (req, res, next) => {
+    const token = req.cookies[cookieName];
+    req.user = null;
+    if (!token) return next();
+    try { req.user = verifyToken(token); } catch (e) {}
     next();
+  };
 };
 
-// Restrict to Admin Only
+const restrictToLoggedInUserOnly = (req, res, next) => {
+  if (!req.user) return res.redirect("/user/signin");
+  next();
+};
+
 const restrictTo = (roles = []) => {
-    return (req, res, next) => {
-        if (!req.user) {
-            return res.redirect("/user/signin");
-        }
-        if (!roles.includes(req.user.role)) {
-            return res.status(403).send("Access Denied: Admins Only");
-        }
-        next();
-    };
+  return (req, res, next) => {
+    if (!req.user) return res.redirect("/user/signin");
+    if (!roles.includes(req.user.role)) return res.status(403).send("Access Denied: Admins Only");
+    next();
+  };
 };
 
-module.exports = { 
-    checkForAuthenticationCookie, 
-    restrictTo, 
-    restrictToLoggedInUserOnly 
+module.exports = {
+  checkForAuthenticationCookie,
+  restrictTo,
+  restrictToLoggedInUserOnly
 };
